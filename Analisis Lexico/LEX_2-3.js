754a20b0bbd9ddd9179025454938548c74c653d3
davidromero8 <112227937+davidromero8@users.noreply.github.com>
2023-10-22 23:10:11 -0600

    Update LEX_2-3.js
    se agrego toUpperCase

--- Analisis Lexico/LEX_2-3.js
+++ Analisis Lexico/LEX_2-3.js
@@ -8,38 +8,47 @@ fs.readFile('sql_text.txt', 'utf8', (err, data) => {
     }
 
 function separarTexto(data, palabraClave) {
-    data = data.trim()
+    const simbolos = ['.', '_', ',', '-', ' ', ';', '*', '+', '/', '=', '(', ')', '<', '>', '!', '%', '$', '@', '&', '|','^', '`', '~', '?', ':', '"', "'", '[', ']','#']
     let guardarPalabras = []
     let palabra = ''
     let id = []
+    let simboloEncontrado = []
+    data = data.trim()
 
     for (let i = 0; i < data.length; i++) {
-        let caracter = data[i]
-    
+        let caracter = data[i]        
+        let temporal = []
+
         if (caracter !== ' ') {
-            palabra += caracter
+            palabra += caracter.toUpperCase()
+        
+          // probando condicion para separar simbolos  
         } else if (palabra !== '') {
-            guardarPalabras.push(palabra)
+            guardarPalabras.push("Palabra Clave => " + palabra)
+
             if (palabraClave[palabra]) {
-                id.push(palabraClave[palabra])
+                id.push(palabra + " => " + palabraClave[palabra])
+                
             } else {
-                id.push(null)
+                id.push(palabra + " => " + 999)
+                
             }
             palabra = ''
-        }
-    }
-    
+
+        } //cierre else if
+    } //cierre for
+
     if (palabra !== '') {
         guardarPalabras.push(palabra)
         if (palabraClave[palabra]) {
             id.push(palabraClave[palabra])
         } else {
-            id.push(null)
+            id.push(999)
         }
-    }
+    } //cierre if
 
     return { palabras: guardarPalabras, ids: id }
-}
+} //cierre funcion separarTexto
 
 /*--------------------------LEXICO 2/3--------------------------------------*/
 
@@ -52,8 +61,7 @@ fs.readFile('sqlkeywords.txt', 'utf8', (err, keyData) => {
 
     // se separa id y palabra clave
     const linea = keyData.split('\n')
-const keywords = []
-const num = []
+    const keywords = []
 
 //dividira laas lineas en 2, una antes de : y despues de :
 for (let i = 0; i < linea.length; i++) {
@@ -64,10 +72,11 @@ for (let i = 0; i < linea.length; i++) {
     if (parts.length === 2) {
         keywords[parts[1].trim()] = parseInt(parts[0])
     }
-}   
+} //cierre for   
 
         const { palabras, ids } = separarTexto(data, keywords)
         console.log(palabras)
         console.log(ids)
-    })
-})
+    }) //cierre readfile sqlkeywords
+}) //cierre readfile sql_text
+
